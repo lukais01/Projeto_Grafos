a158bc6109ac0db782b06824e6f05bb4c2b0f669
Lukais <143547910+lukais01@users.noreply.github.com>
2026-05-25 18:12:26 -0300

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,7 @@
+# Menu principal do sistema
+
+def menu_principal():
+    print("=== Sistema de Centros de Doação ===")
+    print("1 - Visualizar Grafos")
+    print("2 - Analisar Conexões")
+    print("3 - Verificar Grafo")

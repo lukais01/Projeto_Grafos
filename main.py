34d1185dab4bf939cfc5e1f335d702b4abbf01de
Lukais <143547910+lukais01@users.noreply.github.com>
2026-05-25 18:10:48 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# Participantes: 
+# Lucas Akio RA:10425346
+# Diogo Fassina RA:10417030
+# Projeto Teoria dos Grafos
+# Arquivo principal da aplicação
+
+from menu import menu_principal
+
+menu_principal()
